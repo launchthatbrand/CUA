dc6ef71416aff3b46a41a4d53b20f955fe5d70f0
Desmond Tatilian <info@launchthatbrand.com>
2023-04-02 19:53:01 -0400

    layout sidebar

--- packages/app/features/layout.tsx
+++ packages/app/features/layout.tsx
@@ -1,19 +1,12 @@
 import type { PropsWithChildren } from "react";
 import { XStack, YStack, ZStack } from "@my/ui";
+import Sidebar from "@my/ui/src/components/Sidebar/Sidebar";
 
-export const PageLayout = (props: PropsWithChildren) => {
+export const PageLayout = (props: PropsWithChildren<unknown>) => {
   return (
     <>
       <XStack backgroundColor={"$green10Light"}>
-        <YStack
-          className={"left-nav"}
-          backgroundColor={"$pink8Dark"}
-          flexBasis="20%"
-        >
-          <YStack bc="$color" br="$3" p="$2" />
-          <YStack bc="$color" br="$3" p="$2" />
-          <YStack bc="$color" br="$3" p="$2" />
-        </YStack>
+        <Sidebar />
 
         <YStack className={"right-content"} flexBasis="80%">
           <XStack backgroundColor={"$blue10Dark"}>
